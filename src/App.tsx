b0bce2bef9456f17e4c9464537400ea571419f2b
NoKi <novak.kirill@gmail.com>
2024-10-01 10:54:07 +0300

    change titles

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,11 @@ import { useAtom } from 'jotai'
 import { pathAtom } from '@/store/pathStore'
 import { invoke } from '@tauri-apps/api/tauri'
 import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
     const [path, setPath] = useAtom(pathAtom)
+    const [isSuccesed, setIsSuccesed] = useState(false)
 
     const startSorting = async (path: string) => {
         invoke('list_files_in_folder', { folderPath: path })
@@ -15,7 +17,10 @@ function App() {
                     fileList: response,
                     parentFolderPath: path,
                 })
-                    .then(() => console.log('Succses'))
+                    .then(() => {
+                        setIsSuccesed(true)
+                        console.log('Succses')
+                    })
                     .catch((e) => {
                         console.error('Error 2: ', e)
                     })
@@ -24,6 +29,15 @@ function App() {
                 console.error('Error 1: ', e)
             })
     }
+
+    useEffect(() => {
+        if (isSuccesed) {
+            setTimeout(() => {
+                setIsSuccesed(false)
+                setPath(null)
+            }, 5000)
+        }
+    }, [isSuccesed])
 
     return (
         <div className='container flex items-center justify-center m-0 p-0'>
@@ -37,13 +51,13 @@ function App() {
             </div>
             <div className='my-5'>
                 <p className='font-bold'>
-                    Select the folder in which you want to sort the photos to
-                    get the path.
+                    Select the folder in which you want to sort the photos.
                 </p>
                 <p className='font-bold'>
-                    The path needs to start sorting files in the source folder.
+                    We need the path to start sorting files in the source
+                    folder.
                 </p>
-                <p className='font-bold text-red-600'>Let's do it</p>
+                <p className='font-bold text-green-500'>Let's do it</p>
             </div>
             <div className='bg-amber-300 w-screen'>
                 <FolderSelector />
@@ -61,11 +75,22 @@ function App() {
                     <button
                         onClick={() => {
                             setPath(null)
+                            setIsSuccesed(false)
                         }}
                         className='px-4 py-2 bg-white hover:border-green-700 text-green-500 rounded hover:bg-white focus:outline-none focus:ring-2 focus:bg-green-700 focus:ring-opacity-50'
                     >
                         Cancel
                     </button>
+                </div>
+            )}
+            {isSuccesed && (
+                <div className='my-5'>
+                    <h1 className='text-green-500 font-bold text-2xl'>
+                        Congradulation!
+                    </h1>
+                    <h2 className='text-green-500 font-bold text-1xl'>
+                        Files were sorted
+                    </h2>
                 </div>
             )}
         </div>
